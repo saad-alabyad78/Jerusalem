faddd031369029b297fae2d7bf3ceb0a9855995e
saad-alabyad <147769830+saad-alabyad78@users.noreply.github.com>
2024-01-27 02:57:15 +0300

    Update App.cpp

--- miniEngine/src/App.cpp
+++ miniEngine/src/App.cpp
@@ -1,9 +1,11 @@
-﻿#include <GL/glew.h>
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
 #include "Camera.h"
 #include "Window.h"
 #include <Scene.h>
+#include <imgui/imgui.h>
+#include <imgui/imgui_impl_glfw_gl3.h>
 #include <set>
 using namespace std;
 
@@ -43,6 +45,15 @@ int main() {
 	glfwSetInputMode(window->m_WindowID, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
 
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGui_ImplGlfwGL3_Init(window->m_WindowID, true);
+	ImGui::StyleColorsClassic();
+
+	map<string, bool> switches{};
+	switches["fps"] = true;
+	switches["night"] = false;
+	switches["door"] = false;
 
 	while (window->isRunning()) {
 		// per-frame time logic
@@ -50,10 +61,52 @@ int main() {
 		float currentFrame = static_cast<float>(glfwGetTime());
 		deltaTime = currentFrame - lastFrame;
 		lastFrame = currentFrame;
-		
-		scene->Input(deltaTime, mouse_callback);
+
+		ImGui_ImplGlfwGL3_NewFrame();
+
+		map<string, bool> buttons{};
+
+		ImGui::Spacing();
+		ImGui::Spacing();
+		ImGui::Spacing();
+		ImGui::Text("Press 'Tab' to show cursor");
+		if (ImGui::Button("Dome of The Rock", ImVec2(200.0f, 70.0f))) {
+			buttons["po1"] = true;
+		}
+		if (ImGui::Button("Qibli Mosque", ImVec2(200.0f, 70.0f))) {
+			buttons["po2"] = true;
+		}
+		if (ImGui::Button("Sky View", ImVec2(200.0f, 70.0f))) {
+			buttons["po3"] = true;
+		}
+		if (ImGui::RadioButton("First Person", switches["fps"]) || window->key(GLFW_KEY_F5)) {
+			switches["fps"] = !switches["fps"];
+		}
+		buttons["fps"] = switches["fps"];
+		if (ImGui::RadioButton("Night Mode", switches["night"])) {
+			switches["night"] = !switches["night"];
+			if (switches["night"]) {
+				buttons["moon"] = true;
+			}
+			else {
+				buttons["son"] = true;
+			}
+		}
+		if (ImGui::RadioButton("Qibli Door", switches["door"]) || window->key(GLFW_KEY_E)) {
+			switches["door"] = !switches["door"];
+			if (switches["door"]) {
+				buttons["d_open"] = true;
+			}
+			else {
+				buttons["d_close"] = true;
+			}
+		}
+
+		scene->Input(deltaTime, mouse_callback, buttons);
 		scene->Update();
 
+		ImGui::Render();
+		ImGui_ImplGlfwGL3_RenderDrawData(ImGui::GetDrawData());
 
 		window->swapBuffer();
 		window->pollEvents();
